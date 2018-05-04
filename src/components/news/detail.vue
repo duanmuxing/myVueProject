2f76730d21fe100de7fcbc54d7da401ce05d972f
dxw <dxw@aaa.con>
2018-05-04 15:07:21 +0800

    新闻页面完成

--- src/components/news/detail.vue
+++ src/components/news/detail.vue
@@ -1,12 +1,48 @@
 <template>
 	<div class="page">
-		<h1>这是新闻详情</h1>
+		 	<div class="news-title">
+                <h4 v-text="newsInfo.title"></h4>
+                <p class="news-info">
+                    <span>发表时间: {{newsInfo.add_time | dateformat("YYYY年MM月DD日 HH:mm:ss")}}</span>
+                    <span class="mui-pull-right">点击： {{newsInfo.click}}次</span>
+                </p>
+                <hr>
+         	</div>
+            <div class="news-content" v-html="newsInfo.content">
+                
+            </div>
+        <div class="news-comment">
+            <comment :id="$route.params.id"></comment>
+        </div>
 	</div>
 </template>
 
 <script>
+import axios from "axios"
+import dateformat from "@/filters/dateformat"
+import comment from "../common/comment"
 export default{
-	
+	data(){
+		return{
+			newsInfo:{}
+		}
+	},
+	created(){
+		axios({
+			url:"http://www.escook.cn:3000/api/getnew/" + this.$route.params.id
+		}).then(res=>{
+			if(res.data.status==0){
+			console.log(res.data.message[0])
+				this.newsInfo=res.data.message[0]
+			}
+		})
+	},
+	filters:{
+		dateformat
+	},
+	components:{
+		comment
+	}
 }
 </script>
 
--- src/components/news/list.vue
+++ src/components/news/list.vue
@@ -1,12 +1,36 @@
 <template>
 	<div class="page">
-		<h1>这是新闻列表</h1>
+		<ul class="mui-table-view mui-table-view-chevron">
+		    <li v-for="(v,i) in newsList" :key="i" class="mui-table-view-cell mui-media">
+		        <router-link :to="'/news/detail/'+v.id" class="mui-navigate-right">
+		            <img class="mui-media-object mui-pull-left" :src="v.img_url">
+		            <div class="mui-media-body mui-ellipsis">
+		                {{ v.title }}
+		                <p class='mui-ellipsis' v-text="v.zhaiyao"></p>
+		            </div>
+		        </router-link>
+		    </li>
+		</ul>
 	</div>
 </template>
 
 <script>
+import axios from "axios"
 export default{
-	
+	data(){
+		return{
+			newsList:[]
+		}
+	},
+	created(){
+		axios({
+			url:"http://www.escook.cn:3000/api/getnewslist"
+		}).then(res=>{
+			if(res.data.status==0){
+				this.newsList=res.data.message
+			}
+		})
+	}
 }
 </script>
 
